5f3a96897b6a896f8e7b6dc57be7c25950e28593
Harjot Bhatt <BHA210671@spengergasse.at>
2024-09-21 16:04:46 +0200

    FERTIG-Teil2

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,86 +1,195 @@
+// app/routes/login.tsx
 import { useState } from 'react'
 import { Layout } from '~/components/layout'
 import { FormField } from '~/components/form-field'
+import { login, register } from '~/utils/auth.server'
+import { ActionFunction } from '@remix-run/node'
+
+import { useActionData } from '@remix-run/react'
+import { useRef, useEffect } from 'react'
+
+import {  json } from '@remix-run/node'
+import { validateEmail, validateName, validatePassword } from '~/utils/validators.server'
+import {  LoaderFunction, redirect } from '@remix-run/node'
+import {  getUser } from '~/utils/auth.server'
+
+
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData()
+  const action = form.get('_action')
+  const email = form.get('email')
+  const password = form.get('password')
+  let firstName = form.get('firstName')
+  let lastName = form.get('lastName')
+
+  if (typeof action !== 'string' || typeof email !== 'string' || typeof password !== 'string') {
+    return json({ error: `Invalid Form Data`, form: action }, { status: 400 })
+  }
+
+  if (action === 'register' && (typeof firstName !== 'string' || typeof lastName !== 'string')) {
+    return json({ error: `Invalid Form Data`, form: action }, { status: 400 })
+  }
+
+  const errors = {
+    email: validateEmail(email),
+    password: validatePassword(password),
+    ...(action === 'register'
+      ? {
+          firstName: validateName((firstName as string) || ''),
+          lastName: validateName((lastName as string) || ''),
+        }
+      : {}),
+  }
+
+  if (Object.values(errors).some(Boolean))
+    return json({ errors, fields: { email, password, firstName, lastName }, form: action }, { status: 400 })
+
+  switch (action) {
+    case 'login': {
+        return await login({ email, password })
+    }
+    case 'register': {
+        firstName = firstName as string
+        lastName = lastName as string
+        return await register({ email, password, firstName, lastName })
+    }
+    default:
+        return json({ error: `Invalid Form Data` }, { status: 400 });
+  }
+
+}
+
+
 
 export default function Login() {
-    const [action, setAction] = useState('login')
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-        firstName: '',
-        lastName: '',
-    })
+   
+  const [action, setAction] = useState('login')
+  // Updates the form data when an input changes
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, field: string) => {
+    setFormData(form => ({ ...form, [field]: event.target.value }))
+  }
 
-    // Updates the form data when an input changes
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, field: string) => {
-        setFormData(form => ({ ...form, [field]: event.target.value }))
+  interface ActionData {
+    errors?: {
+      email?: string;
+      password?: string;
+      firstName?: string;
+      lastName?: string;
+    };
+    fields?: {
+      email: string;
+      password: string;
+      firstName: string;
+      lastName: string;
+    };
+    error?: string;
+  }
+  
+  const actionData = useActionData<ActionData>();
+  // 2
+  const firstLoad = useRef(true)
+  const [errors, setErrors] = useState(actionData?.errors || {})
+  const [formError, setFormError] = useState(actionData?.error || '')
+  // 3
+  const [formData, setFormData] = useState({
+    email: actionData?.fields?.email || '',
+    password: actionData?.fields?.password || '',
+    firstName: actionData?.fields?.lastName || '',
+    lastName: actionData?.fields?.firstName || '',
+  })
+  
+
+  useEffect(() => {
+    if (!firstLoad.current) {
+      const newState = {
+        email: actionData?.fields?.email || '',
+        password: actionData?.fields?.password || '',
+        firstName: actionData?.fields?.firstName || '',
+        lastName: actionData?.fields?.lastName || '',
+      };
+      setFormData(newState);
+  
+      // Only reset the errors if there are no errors in actionData
+      if (!actionData?.errors) {
+        setErrors({});
+        setFormError('');
+      }
     }
+  }, [action, actionData]);
+  
+  useEffect(() => {
+    if (!firstLoad.current) {
+      setFormError('')
+    }
+  }, [formData])
 
-    return (
-        <Layout>
-            <div className="h-full justify-center items-center flex flex-col gap-y-4">
-                <h2 className="text-5xl font-extrabold text-yellow-300">Welcome to Kudos!</h2>
-                <p className="font-semibold text-slate-300">
-                    {action === 'login' ? 'Log In To Give Some Praise!' : 'Sign Up To Get Started!'}
-                </p>
+  useEffect(() => { firstLoad.current = false }, [])
 
-                <form method="POST" className="rounded-2xl bg-gray-200 p-6 w-96">
-                    {/* Email Field */}
-                    <FormField
-                        htmlFor="email"
-                        label="Email"
-                        value={formData.email}
-                        onChange={e => handleInputChange(e, 'email')}
-                    />
-                    {/* Password Field */}
-                    <FormField
-                        htmlFor="password"
-                        type="password"
-                        label="Password"
-                        value={formData.password}
-                        onChange={e => handleInputChange(e, 'password')}
-                    />
 
-                    {/* Only show these fields if action is "register" */}
-                    {action === 'register' && (
-                        <>
-                            {/* First Name Field */}
-                            <FormField
-                                htmlFor="firstName"
-                                label="First Name"
-                                value={formData.firstName}
-                                onChange={e => handleInputChange(e, 'firstName')}
-                            />
-                            {/* Last Name Field */}
-                            <FormField
-                                htmlFor="lastName"
-                                label="Last Name"
-                                value={formData.lastName}
-                                onChange={e => handleInputChange(e, 'lastName')}
-                            />
-                        </>
-                    )}
+  return (
+    <Layout>
+      <div className="h-full justify-center items-center flex flex-col gap-y-4">
+        <h2 className="text-5xl font-extrabold text-yellow-300">Welcome to Kudos!</h2>
+        <p className="font-semibold text-slate-300">
+          {action === 'login' ? 'Log In To Give Some Praise!' : 'Sign Up To Get Started!'}
+        </p>
 
-                    {/* Submit Button */}
-                    <div className="w-full text-center">
-                        <input
-                            type="submit"
-                            className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
-                            value={action === 'login' ? 'Sign In' : 'Register'}
-                        />
-                    </div>
-                </form>
-            </div>
+        <form method="POST" className="rounded-2xl bg-gray-200 p-6 w-96">
+        <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">{formError}</div>
+          <FormField
+            htmlFor="email"
+            label="Email"
+            value={formData.email}
+            onChange={e => handleInputChange(e, 'email')}
+            error={errors?.email}
+          />
+          <FormField
+            htmlFor="password"
+            type="password"
+            label="Password"
+            value={formData.password}
+            onChange={e => handleInputChange(e, 'password')}
+            error={errors?.password}
+          />
 
-            <div className="h-full justify-center items-center flex flex-col gap-y-4">
-                {/* Button to switch between login and register */}
-                <button
-                    onClick={() => setAction(action === 'login' ? 'register' : 'login')}
-                    className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
-                >
-                    {action === 'login' ? 'Sign Up' : 'Sign In'}
-                </button>
-            </div>
-        </Layout>
-    )
+{action === 'register' && (
+            <>
+              <FormField
+                htmlFor="firstName"
+                label="First Name"
+                onChange={e => handleInputChange(e, 'firstName')}
+                value={formData.firstName}
+                error={errors?.firstName}
+              />
+              <FormField
+                htmlFor="lastName"
+                label="Last Name"
+                onChange={e => handleInputChange(e, 'lastName')}
+                value={formData.lastName}
+                error={errors?.lastName}
+              />
+            </>
+          )}
+
+          <div className="w-full text-center">
+          <button type="submit" name="_action" value={action} className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1">
+           {
+             action === 'login' ? "Sign In" : "Sign Up"
+           }
+        </button>
+          </div>
+        </form>
+      </div>
+      <div className="h-full justify-center items-center flex flex-col gap-y-4">
+      <button
+        onClick={() => setAction(action == 'login' ? 'register' : 'login')}
+        className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+      >
+        {action === 'login' ? 'Sign Up' : 'Sign In'}
+      </button>
+      
+      </div>
+    </Layout>
+  )
 }
---
+++ prisma/migrations/20240919142817_update_schema/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
